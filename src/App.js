3e243d69e5ae7ce72d145d14c812524018a2e04e
sakib1416 <sakib1416@gmail.com>
2021-03-19 10:29:47 +0100

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import { Navbar, Nav, Form} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <h1>Assignment 9</h1>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Easy-Rides</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+          </Nav>
+          <Form inline>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Destination</Nav.Link>
+            <Nav.Link href="#home">Blog</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
+            <Nav.Link href="#link">Login</Nav.Link>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
